79b048fb5b22d17c9cdbb1194bc83937a518bda0
Leslie Delval <80229002+LeslieDelval@users.noreply.github.com>
2023-04-02 14:24:53 -0500

    Update camera_cv2.py
    Display of 2 cameras

--- camera_cv2.py
+++ camera_cv2.py
@@ -1,12 +1,16 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(0)
+cam2 = cv2.VideoCapture(1)
 
 while True:
-        ret, image = cam.read()
-        cv2.imshow('Imagetest',image)
+        ret1, image1 = cam1.read()
+        ret2, image2 = cam2.read()
+        cv2.imshow('Front View',image1)
+        cv2.imshow('Back View',image2)
         k = cv2.waitKey(1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-cam.release()
+cam1.release()
+cam2.release()
 cv2.destroyAllWindows()
