45d749de08b97f491dc4a66e6ff215ac59fa53ab
Leslie Delval <80229002+LeslieDelval@users.noreply.github.com>
2023-04-03 12:34:12 -0500

    Update 2cameras.py
    Resize cameras 640x360

--- 2cameras.py
+++ 2cameras.py
@@ -6,8 +6,15 @@ cam2 = cv2.VideoCapture(2)
 while True:
         ret1, image1 = cam1.read()
         ret2, image2 = cam2.read()
+        
+        cv2.namedWindow('Front View', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Front View', 640, 360)
         cv2.imshow('Front View',image1)
+        
+        cv2.namedWindow('Back', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Back View', 640, 360)        
         cv2.imshow('Back View',image2)
+        
         k = cv2.waitKey(1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
